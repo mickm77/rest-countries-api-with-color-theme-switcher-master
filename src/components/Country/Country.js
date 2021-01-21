083f4612c7e6792ec61205eb41bd954c03233dbf
mick mcmahon <mick.mcmahon@gmail.com>
2021-01-21 11:04:42 +0000

    Removes unused defenitions

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,10 +1,5 @@
 
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Country = ({ name, flag, population, region, capital, alpha3Code }) => {
 
